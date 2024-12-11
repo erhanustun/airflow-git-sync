673995440e0452a478f3c6227204884a2266f263
Erhan Üstün <erhan.ustun.marun@hotmail.com>
2024-12-11 21:04:29 +0300

    update

--- dags/clean_data_dag.py
+++ dags/clean_data_dag.py
@@ -20,14 +20,26 @@ default_args = {
 }
 
 with DAG('dirty_data_clean', default_args=default_args, schedule_interval='@once', catchup=False) as dag:
-    t1 = SSHOperator(
+    t1 = BashOperator(
+    task_id="scp_python_scrips",
+    bash_command=f"sshpass -v -p {ssh_train_password} scp -o StrictHostKeyChecking=no -r 
+    /opt/airflow/dags/airflow-git-sync/python_scripts ssh_train@spark_client:/home/ssh_train/")
+    
+    t2 = SSHOperator(task_id='run_python', 
+                    command=f"python /home/ssh_train/python_scripts/clean_data_dag.py -t {tmdb_token} -c {SQLALCHEMY_DATABASE_URL}",
+                    ssh_conn_id='spark_ssh_conn',
+                    cmd_timeout=600)
+
+    '''t2 = SSHOperator(
     task_id="dirty_data_clean",
     command=f"""source /dataops/airflowenv/bin/activate && 
             python /dataops/dirty_data_clean.py""",
      execution_timeout=timedelta(minutes=20),  # Set the execution timeout to 10 minutes (600 seconds)
     retries=3,
-    ssh_conn_id='spark_ssh_conn')
+    ssh_conn_id='spark_ssh_conn')'''
 
+
+    t1 >> t2
     
 
 
