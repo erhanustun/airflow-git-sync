403f4ab0c4f6009083b8fb672d3d9f3d2bcbf45c
Erhan Üstün <erhan.ustun.marun@hotmail.com>
2024-12-11 21:35:55 +0300

    u

--- dags/clean_data_dag.py
+++ dags/clean_data_dag.py
@@ -19,11 +19,10 @@ default_args = {
 }
 
 ssh_train_password = Variable.get("ssh_train_password")
-SQLALCHEMY_DATABASE_URL = Variable.get("SQLALCHEMY_DATABASE_URL")
 
 with DAG('dirty_data_clean', default_args=default_args, schedule_interval='@once', catchup=False) as dag:
     t1 = BashOperator(
-    task_id="scp_python_scrips",
+    task_id="scp_python_scripts",
     bash_command=f"sshpass -v -p {ssh_train_password} scp -o StrictHostKeyChecking=no -r 
     /opt/airflow/dags/airflow-git-sync ssh_train@spark_client:/home/ssh_train/python_scripts/")
     
